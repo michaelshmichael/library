cf13908b2e149f4e761eefe3be858d741c99d46a
michaelshmichael <michaeldcjohnson@gmail.com>
2020-09-28 19:36:52 +0600

    made basic functionality of adding books. removed plant data

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-let myLibrary = [
-{title: "A Gentleman In Moscow", author: "Amor Towles", pages: 462, read: true},
-{title: "Total Recall", author: "Arnold Schwarzenegger", pages: 390, read: false} ,
-{title: "Any Human Heart", author: "William Boyd", pages: 510, read: true}
-];
-
+let myLibrary = [];
 const bookDisplay = document.getElementById("bookDisplay");
 const addBook = document.getElementById("addBook");
 const inputTable = document.querySelector(".inputTable");
+const submitButton = document.getElementById("submitButton");
 
+// book constructor
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+};
 
+// showing books
 function displayAllBooks(){
   for(i = 0; i < myLibrary.length; i++){
-    const book = document.createElement('div');
-    book.classList.add('displayedBook')
-    bookDisplay.appendChild(book);
-    const title = myLibrary[i].title;
-    const author = myLibrary[i].author;
-    const pages = myLibrary[i].pages;
-    const read = myLibrary[i].read;
-    book.textContent = title + author + pages + read
+  const book = document.createElement('div');
+  book.classList.add('displayedBook')
+  bookDisplay.appendChild(book);
+  const title = myLibrary[i].title;
+  const author = myLibrary[i].author;
+  const pages = myLibrary[i].pages;
+  const read = myLibrary[i].read;
+  book.textContent = title + author + pages + read
   }
 }
 
-displayAllBooks();
-
-function Book() {
-  // the constructor...
-}
-
 addBook.addEventListener("click", function(){
+  inputTable.reset();
   inputTable.classList.toggle("inputTableActive");
 });
 
-function addBookToLibrary() {
-
+// adds a book to the library
+function addBookToLibrary(ev){
+  ev.preventDefault();
+  let newBook = new Book(document.getElementById("title").value,
+                         document.getElementById("author").value, 
+                         document.getElementById("pages").value, 
+                         document.getElementById("read").value);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+  inputTable.classList.toggle("inputTableActive")
+  displayAllBooks();
 }
+
+submitButton.addEventListener("click", addBookToLibrary);
