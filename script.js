1607c8721d5e608a6ba68d6d389f24c05502236a
michaelshmichael <michaeldcjohnson@gmail.com>
2020-09-28 21:43:10 +0600

    stylised and allowed for smooth input with user books

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const addBook = document.getElementById("addBook");
 const inputTable = document.querySelector(".inputTable");
 const submitButton = document.getElementById("submitButton");
 
+
 // book constructor
 function Book(title, author, pages, read) {
   this.title = title;
@@ -14,30 +15,57 @@ function Book(title, author, pages, read) {
 
 // showing books
 function displayAllBooks(){
-  for(i = 0; i < myLibrary.length; i++){
-  const book = document.createElement('div');
-  book.classList.add('displayedBook')
-  bookDisplay.appendChild(book);
-  const title = myLibrary[i].title;
-  const author = myLibrary[i].author;
-  const pages = myLibrary[i].pages;
-  const read = myLibrary[i].read;
-  book.textContent = title + author + pages + read
+
+  for(i = myLibrary.length - 1; i < myLibrary.length; i++){
+    const book = document.createElement('div');
+    book.classList.add('displayedBook')
+    bookDisplay.appendChild(book);
+      for(j = 0; j < 4; j++){
+        const detail = document.createElement('div');
+        detail.classList.add('displayedDetails');
+        detail.setAttribute("id", "detail_" + i + j);
+        book.appendChild(detail);
+      }
+    document.getElementById("detail_" + i + "0").textContent = "Title: " + myLibrary[i].title; 
+    document.getElementById("detail_" + i + "1").textContent = "Author: " + myLibrary[i].author;
+    document.getElementById("detail_" + i + "2").textContent = "No. of Pages: " + myLibrary[i].pages;
+    if(myLibrary[i].read === false){
+      document.getElementById("detail_" + i + "3").textContent = "Unread";   
+    } else {
+      document.getElementById("detail_" + i + "3").textContent = "Read";
+    }
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+    deleteButton.setAttribute("id", "delete" + i)
+    deleteButton.textContent = "Delete Book"
+    book.appendChild(deleteButton);
   }
 }
+
 
 addBook.addEventListener("click", function(){
   inputTable.reset();
   inputTable.classList.toggle("inputTableActive");
 });
 
+let deleteButtons = document.getElementsByClassName("deleteButton");
+let deleteButtonsArray = Array.from(deleteButtons);
+
+// deleteButton.forEach(function(e){
+//   e.addEventListener("click", function(){
+
+//   })
+// })
+
+
 // adds a book to the library
+
 function addBookToLibrary(ev){
   ev.preventDefault();
   let newBook = new Book(document.getElementById("title").value,
                          document.getElementById("author").value, 
-                         document.getElementById("pages").value, 
-                         document.getElementById("read").value);
+                         parseInt(document.getElementById("pages").value), 
+                         document.getElementById("read").checked);
   myLibrary.push(newBook);
   console.log(myLibrary);
   inputTable.classList.toggle("inputTableActive")
