c1a18a85cd7dd515379fdcd3938d93f14dc7c143
michaelshmichael <michaeldcjohnson@gmail.com>
2020-10-03 12:49:55 +0600

    added local storage but needs work to always refresh

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let myLibrary = [];
+let myLibrary
+if (localStorage.getItem('items')) {
+  myLibrary = JSON.parse(localStorage.getItem('items'))
+} else {
+  myLibrary = []
+}
+localStorage.setItem('items', JSON.stringify(myLibrary));
+const data = JSON.parse(localStorage.getItem('items'));
 const bookDisplay = document.getElementById("bookDisplay");
 const addBook = document.getElementById("addBook");
 const inputTable = document.querySelector(".inputTable");
 const submitButton = document.getElementById("submitButton");
-let data = JSON.parse(localStorage.getItem('items'))
 
 
 // book constructor
@@ -14,13 +20,9 @@ function Book(title, author, pages, read) {
   this.read = read;
 };
 
-// creating book cards and applying values to them
-function displayAllBooks(){
-
-  // creating book cards
-  for(i = myLibrary.length - 1; i < myLibrary.length; i++){
+for(i = 0; i < data.length; i++){
     const book = document.createElement('div');
-    //book.classList.add('displayedBook')
+   //book.classList.add('readBook')
     book.setAttribute("data-index", i);
     bookDisplay.appendChild(book);
 
@@ -32,11 +34,11 @@ function displayAllBooks(){
               book.appendChild(detail);
             }
 
-    // creating the text values and whether read or not for the books
-    document.getElementById("detail_" + i + "0").textContent = "Title: " + myLibrary[i].title; 
-    document.getElementById("detail_" + i + "1").textContent = "Author: " + myLibrary[i].author;
-    document.getElementById("detail_" + i + "2").textContent = "No. of Pages: " + myLibrary[i].pages;
-      if(myLibrary[i].read === false){
+    //creating the text values and whether read or not for the books
+    document.getElementById("detail_" + i + "0").textContent = "Title: " + data[i].title; 
+    document.getElementById("detail_" + i + "1").textContent = "Author: " + data[i].author;
+    document.getElementById("detail_" + i + "2").textContent = "No. of Pages: " + data[i].pages;
+      if(data[i].read === false){
         document.getElementById("detail_" + i + "3").textContent = "Unread";
         book.classList.add('unreadBook')  
       } else {
@@ -44,7 +46,7 @@ function displayAllBooks(){
         book.classList.add('readBook');
       }
 
-    // creating the delete button and setting its values
+    //creating the delete button and setting its values
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("deleteButton");
     deleteButton.setAttribute("id", "delete" + i)
@@ -54,7 +56,7 @@ function displayAllBooks(){
     deleteButton.addEventListener("click", function(e){
       removeBook(e)
     })
-    
+
     const toggleReadButton = document.createElement("button");
     toggleReadButton.classList.add("toggleRead");
     toggleReadButton.setAttribute("data-index", i);
@@ -62,9 +64,21 @@ function displayAllBooks(){
     book.appendChild(toggleReadButton);
     toggleReadButton.addEventListener("click", function(e){
       toggleRead(e);
-    })
-    
-  }
+  })
+};
+
+
+submitButton.addEventListener("click", addBookToLibrary);
+
+function addBookToLibrary(e){
+  e.preventDefault();
+  let newBook = new Book(document.getElementById("title").value,
+                         document.getElementById("author").value, 
+                         parseInt(document.getElementById("pages").value), 
+                         document.getElementById("read").checked);
+  myLibrary.push(newBook);
+  localStorage.setItem('items', JSON.stringify(myLibrary));
+  inputTable.classList.toggle("inputTableActive")
 };
 
 // removing book from library
@@ -118,35 +132,5 @@ addBook.addEventListener("click", function(){
   inputTable.classList.toggle("inputTableActive");
 });
 
-function addBookToLibrary(e){
-  e.preventDefault();
-  let newBook = new Book(document.getElementById("title").value,
-                         document.getElementById("author").value, 
-                         parseInt(document.getElementById("pages").value), 
-                         document.getElementById("read").checked);
-  myLibrary.push(newBook);
 
-
-
-  localStorage.setItem('items', JSON.stringify(myLibrary))
-
-
-
-  console.log(myLibrary);
-  inputTable.classList.toggle("inputTableActive")
-  displayAllBooks();
-};
-
-submitButton.addEventListener("click", addBookToLibrary);
-
-
-// storage
-
-// function populateStorage() {
-//   localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
-//   localStorage.setItem('font', document.getElementById('font').value);
-//   localStorage.setItem('image', document.getElementById('image').value);
-
-//   setStyles();
-// }
 
